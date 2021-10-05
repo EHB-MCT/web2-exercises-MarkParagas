e7cf5c2cfeab3f92010021c1a5f13eb6729bf35c
markparagas <mark.paragas@student.ehb.be>
2021-10-05 16:19:23 +0200

    Fetch with fetch
    Adjusting the html

--- S2_Exercise/js/index.js
+++ S2_Exercise/js/index.js
@@ -1,6 +1,27 @@
 "use strict";
 
+let list = [];
+let pokemons = [];
+
+fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+    .then(resp => resp.json())
+    .then(data => {
+        list = data.results;
+        list.forEach(poke => {
+            fetch(poke.url)
+                .then(response => response.json())
+                .then(pokeData => {
+                    pokemons.push(pokeData)
+                });
+        });
+    });
+
 window.onload = function () {
+    setTimeout(buildlist, 3000);
 
-};
-
+    function buildlist() {
+        console.log(pokemons);
+    }
+    // build list with html and templates literals
+    // add to html
+}
